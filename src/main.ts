90f0c874d2cbf3e31eaf759ed66d266d3b9561af
liujialei_ext <liujialei_ext@cop.cmsoft.com.cn>
2022-07-29 22:14:00 +0800

    配置1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import "@/styles/element/index.scss"
+// import "@/styles/element/index.scss"
 
-import "element-plus/theme-chalk/src/message.scss"
-import 'uno.css'
+// import "element-plus/theme-chalk/src/message.scss"
+// import 'uno.css'
 
 import App from './App.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
 import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// plugins
-import legacy from '@vitejs/plugin-legacy'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
 import { resolve } from 'path'
 
 //封装公共方法
 import { wrapperEnv } from './build/utils'
+import { defaultUnocss } from './build/config/unocss'
+import { defaultPlugins } from './build/config/plugins'
+import { cssPreprocessorOptions } from './build/config/css'
 
-//添加element UI Plus 添加按需自动引入组件库
-
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Unocss from 'unocss/vite'
-import {
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
 
 function pathResolve(dir:string) {
 	 return resolve(__dirname, dir)
@@ -33,83 +19,49 @@ export default defineConfig(({ command, mode }) => {
 
 	const env = loadEnv(mode, root);
 	const viteEnv = wrapperEnv(env);
+	const isBuild = command === 'serve';
+	const { VITE_PORT, VITE_PUBLIC_PATH, VITE_DROP_CONSOLE } = viteEnv;
 	// console.log('viteEnv',viteEnv);
-	if (command === 'serve') {
-    return {
-      base:'./',
-			root,
-			resolve:{
-				alias:[
-					{
-						find:'@',
-						replacement: pathResolve('src')
-					},
-				]
-			},
-			css:{
-				modules:{
-					// css模块化 文件以.module.[css|less|scss]结尾
-					scopeBehaviour: 'global',
-					generateScopedName: '[name]__[local]___[hash:base64:5]',
-					hashPrefix: 'prefix'
+	
+	return {
+		base:VITE_PUBLIC_PATH,
+		root,
+		// cacheDir: 'cache/deps',
+		resolve:{
+			alias:[
+				{
+					find:'@',
+					replacement: pathResolve('src')
 				},
-				preprocessorOptions:{
-					scss: {
-						// additionalData: `@use "@/styles/element/index.scss" as *;`,
-						// additionalData: `@import '@src/styles/variables.scss';@use "@/styles/element/index.scss" as *;`,
-					}
-				},
+			]
+		},
+		css:{
+			modules:{
+				scopeBehaviour: 'global',
+				generateScopedName: '[name]__[local]___[hash:base64:5]',
+				hashPrefix: 'prefix'
 			},
-			plugins: [
-				vue(),
-				vueJsx({
-					// options are passed on to @vue/babel-plugin-jsx
-				}),
-				legacy({
-					targets: ['defaults', 'not IE 11']
-				}),
-				AutoImport({
-				// imports: ["vue", "vue-router"],
-				resolvers: [ElementPlusResolver(
-					{
-						// importStyle: "sass"
-					}
-				)],
-				dts: "auto-imports.d.ts"
-				}),
-				Components({
-					// allow auto load markdown components under `./src/components/`
-					extensions: ['vue', 'md'],
-					// allow auto import and register components used in markdown
-					include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-					resolvers: [ElementPlusResolver({
-						importStyle: 'sass'
-					})],
-					dts: 'src/components.d.ts'  // 按需加载的文件夹['src/components']
-				}),
-				Unocss({
-					presets: [
-						presetUno(),
-						presetAttributify(),
-						presetIcons({
-							scale: 1.2,
-							warn: true
-						}),
-					],
-					transformers: [
-						transformerDirectives(),
-						transformerVariantGroup()
-					]
-				})
-			],
-			build:{
-				
-			}
-    }
-  } else {
-    // command === 'build'
-    return {
-      // build 独有配置
-    }
-  }
+			preprocessorOptions:cssPreprocessorOptions,
+		},
+		plugins: defaultPlugins,
+		esbuild: {
+			pure: VITE_DROP_CONSOLE ? ['console.log', 'debugger'] : [],
+		},
+		server:{
+      host: true,
+      port: VITE_PORT,
+			open: false,
+			proxy: {
+				'/api': {
+					target: "http://127.0.0.1:8800",
+					changeOrigin: true,
+					ws: true,
+					rewrite: (path: string) => path.replace(/^\/api/, '')
+				}
+			},
+		},
+		build:{
+			
+		}
+	}
 })
